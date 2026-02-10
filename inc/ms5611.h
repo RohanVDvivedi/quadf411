63d366f3e90711a604607b63cd39700eb258e431
RohanVDvivedi <rohandvivedi@gmail.com>
2026-02-10 21:34:38 +0530

    started implementing state machine for ms5611

--- inc/ms5611.h
+++ inc/ms5611.h
@@ -10,10 +10,13 @@ typedef enum ms5611_state ms5611_state;
 enum ms5611_state
 {
 	MS5611_NOT_STARTED = 0, // initial state
-	MS5611_READ_QUEUED = 1,
-	MS5611_READ_IN_PROGRESS = 2,
-	MS5611_READ_COMPLETE = 3,
-	MS5611_WAIT_FOR_PERIOD = 4,
+
+	MS5611_COMMAND_QUEUED = 1,
+	MS5611_COMMAND_IN_PROGRESS = 2,
+	MS5611_COMMAND_COMPLETED = 3,
+	MS5611_READ_QUEUED = 4,
+	MS5611_READ_IN_PROGRESS = 5,
+	MS5611_READ_COMPLETED = 6,
 };
 
 typedef struct ms5611 ms5611;
@@ -24,24 +27,25 @@ struct ms5611
 	uint8_t i2c_addr;
 
 	ms5611_state state; // current sensor state
+	int state_data_type;
 
 	dpipe* i2c_queue; // disable interrupts before accessing this
 
-	uint16_t C[6]; // PROM data
+	uint16_t C[7]; // PROM data, first element is not used
 
 	uint32_t last_read_in_millis;
 
 	uint32_t read_period_in_millis; // period in millis to be allowed to read the next record
 
-	uint8_t read_D1[3];
-	uint8_t read_D2[3];
+	uint8_t read_buffer_D[3][3]; // buffers for read_buffer_D[1] and read_buffer_D[2]
+	// we will waste the 0th element
 
 	double data;
 };
 
 // initialize the module and makes sure it is present on the bus
 // returns true if present on the bus
-int init_ms5611(ms5611* mod_baro, I2C_HandleTypeDef* hi2c, uint8_t i2c_addr, dpipe* i2c_queue, uint32_t period_in_millis);
+int init_ms5611(ms5611* mod_baro, I2C_HandleTypeDef* hi2c, uint8_t i2c_addr, dpipe* i2c_queue);
 
 // call on i2c txcplt
 int maybe_data_ready_ms5611(ms5611* mod_baro);
--- src/ms5611.c
+++ src/ms5611.c
@@ -1,1 +1,80 @@
 #include<ms5611.h>
+
+int init_ms5611(ms5611* mod_baro, I2C_HandleTypeDef* hi2c, uint8_t i2c_addr, dpipe* i2c_queue)
+{
+	mod_baro->hi2c = hi2c;
+	mod_baro->i2c_addr = i2c_addr;
+	mod_baro->state = MS5611_NOT_STARTED;
+	mod_baro->state_data_type = 1;
+	mod_baro->i2c_queue = i2c_queue;
+	mod_baro->last_read_in_millis = HAL_GetTick();
+	mod_baro->read_period_in_millis = 10;
+
+	if(HAL_I2C_IsDeviceReady(hi2c, i2c_addr << 1, 3, 100) != HAL_OK)
+		return 0;
+
+	// read prom
+	for(int i = 1; i <= 6; i++)
+	{
+		uint8_t buffer[2];
+		HAL_I2C_Mem_Read(hi2c, (mod_baro->i2c_addr) << 1, 0xa0 + i * 2, I2C_MEMADD_SIZE_8BIT, buffer, 2, HAL_MAX_DELAY);
+		mod_baro->C[i] = (((uint16_t)buffer[0]) << 8) | ((uint16_t)buffer[1]);
+	}
+
+	return 1;
+}
+
+int maybe_data_ready_ms5611(ms5611* mod_baro)
+{
+	if(mod_baro->state != MS5611_COMMAND_IN_PROGRESS && mod_baro->state != MS5611_READ_IN_PROGRESS)
+		return 0;
+	uint8_t buffer[1];
+	cy_uint bytes_read = peek_from_dpipe(mod_baro->i2c_queue, buffer, 1, ALL_OR_NONE);
+	if(bytes_read > 0 && buffer[0] == mod_baro->i2c_addr)
+	{
+		read_from_dpipe(mod_baro->i2c_queue, buffer, 1, ALL_OR_NONE);
+		mod_baro->state++; // change to it's corresponding completed state
+		return 1;
+	}
+	return 0;
+}
+
+double get_ms5611(ms5611* mod_baro, int* new_data_arrived)
+{
+	switch(mod_baro->state)
+	{
+		case MS5611_NOT_STARTED :
+		{
+
+		}
+		case MS5611_COMMAND_QUEUED :
+		{
+
+		}
+		case MS5611_COMMAND_IN_PROGRESS :
+		{
+			// do nothing
+			break;
+		}
+		case MS5611_COMMAND_COMPLETED :
+		{
+
+		}
+		case MS5611_READ_QUEUED :
+		{
+
+		}
+		case MS5611_READ_IN_PROGRESS :
+		{
+			// do nothing
+			break;
+		}
+		case MS5611_READ_COMPLETED :
+		{
+
+		}
+	}
+
+	(*new_data_arrived) = 0;
+	return 0;
+}
